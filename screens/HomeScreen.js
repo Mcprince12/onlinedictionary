676689ef8962d9e61a6eb70b640ea3837322eedc
Mcprince12 <sohumtrip@gmail.com>
2020-12-30 10:43:28 +0200

    2nd submission

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -16,35 +16,19 @@ export default class HomeScreen extends React.Component{
 
     getWord=(word)=>{
         var searchKeyword=word.toLowerCase();
-        var url="https://rupinwhitehatjr.github.io/dictionary/"+searchKeyword+".json"
+        var url="https://api.dictionaryapi.dev/api/v2/entries/en/"+word
         return fetch(url)
         .then((data)=>{
-            if(data.status===200){
                 return data.json()
-            }else {
-                return null
-            }
         })
     .then(
         (response)=>{
-            var responseObject=response
-
-            if(responseObject){
-                var wordData=responseObject.definitions[0]
-                var definition=wordData.description
-                var lexicalCategory=wordData.wordType
-
-                this.setState({
-                    word:this.state.text,
-                    definition:definition,
-                    lexicalCategory:lexicalCategory
-                })
-            }else{
-                this.setState({
-                    word:this.state.text,
-                    definition:"Not Found",
-                })
-            }
+            var word=response[0].word
+            var definition = response[0].meanings[0].definitions[0].definition;
+            this.setState({
+                word:word.trim(),
+                definition:definition.trim()
+            })
         }
     )
     }
